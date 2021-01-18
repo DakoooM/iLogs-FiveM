a40746a8167d51e84b04251511505faf7a8de402
Giovani C <60287158+DakoooM@users.noreply.github.com>
2021-01-18 17:03:45 +0100

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -3,7 +3,7 @@ iLogs = {}
 iLogs.FooterLogo = "https://cdn.discordapp.com/attachments/800533844674740255/800615379130515456/Logodakomjpeg.png"
 iLogs.ServerName = "DakoM#0001"
 
-iLogs.Connecting = "https://discord.com/api/webhooks/800533857173635143/Hc_UB4os8xcQ-P4yuN2uomQIYyg6Cd2brYqxmKD35ua4eSeXHSIp4KJog5HDKdINaaNO"
-iLogs.Deconnected = "https://discord.com/api/webhooks/800533857173635143/Hc_UB4os8xcQ-P4yuN2uomQIYyg6Cd2brYqxmKD35ua4eSeXHSIp4KJog5HDKdINaaNO"
-iLogs.ResourceStart = "https://discord.com/api/webhooks/800533857173635143/Hc_UB4os8xcQ-P4yuN2uomQIYyg6Cd2brYqxmKD35ua4eSeXHSIp4KJog5HDKdINaaNO"
-iLogs.ResourceStop = "https://discord.com/api/webhooks/800533857173635143/Hc_UB4os8xcQ-P4yuN2uomQIYyg6Cd2brYqxmKD35ua4eSeXHSIp4KJog5HDKdINaaNO"
+iLogs.Connecting = ""
+iLogs.Deconnected = ""
+iLogs.ResourceStart = ""
+iLogs.ResourceStop = ""
